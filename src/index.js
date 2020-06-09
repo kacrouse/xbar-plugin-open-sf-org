e1a06326446d68c55696413e9ba1822579857bdd
Kyle Crouse <crousek@us.ibm.com>
2020-06-09 17:24:58 -0600

    non-scratch orgs and scratch orgs in a single list

--- src/index.js
+++ src/index.js
@@ -4,29 +4,24 @@ import templateImage from /* preval */ "./get-template-image";
 import { findExecutable, runCommand } from "./util";
 
 const sfdx = findExecutable("sfdx");
-const orgList = JSON.parse(runCommand(`${sfdx} force:org:list --json`));
+const orgListOutput = JSON.parse(runCommand(`${sfdx} force:org:list --json`));
+const orgs = [
+  ...orgListOutput.result.nonScratchOrgs,
+  ...orgListOutput.result.scratchOrgs,
+];
 
-const orgOption = (org) => ({
-  text: org.alias,
-  bash: sfdx,
-  param1: "force:org:open",
-  param2: "--targetusername",
-  param3: org.username,
-});
-
-bitbar(
-  [
-    {
-      templateImage,
-      text: "",
-    },
-    bitbar.separator,
-    ...orgList.result.nonScratchOrgs.map(orgOption),
-    bitbar.separator,
-    {
-      text: "Scratch Orgs",
-    },
-    ...orgList.result.scratchOrgs.map(orgOption),
-  ],
-  { terminal: false }
-);
+bitbar([
+  {
+    templateImage,
+    text: "",
+  },
+  bitbar.separator,
+  ...orgs.map((org) => ({
+    text: org.alias,
+    bash: sfdx,
+    param1: "force:org:open",
+    param2: "--targetusername",
+    param3: org.username,
+    terminal: false,
+  })),
+]);
