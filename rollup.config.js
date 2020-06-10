0066cee108742bcd16615e28568e54575f057bac
Kyle Crouse <crousek@us.ibm.com>
2020-06-10 17:18:21 -0600

    add bitbar metadata

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import commonjs from "@rollup/plugin-commonjs";
 import path from "path";
 import babel from "@rollup/plugin-babel";
 import builtinModules from "builtin-modules";
+import fs from "fs";
 
 export default {
   input: "src/index.js",
@@ -24,6 +25,10 @@ export default {
         ].join("\n"),
     },
     babel({ babelHelpers: "bundled" }),
+    {
+      name: "bitbar-metadata-banner",
+      banner: fs.readFileSync(path.resolve(__dirname, "bitbar-metadata.js")),
+    },
   ],
   external: builtinModules,
 };
