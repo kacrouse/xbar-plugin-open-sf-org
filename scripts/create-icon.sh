903d432e24b9bf545a74c5496fd36ddbe1fea092
Kyle Crouse <crousek@us.ibm.com>
2020-06-11 17:54:13 -0600

    comments broke the icon script :grimace:

--- scripts/create-icon.sh
+++ scripts/create-icon.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-curl -s https://c1.sfdcstatic.com/content/dam/web/en_us/www/images/styleguide/Logos/Salesforce_Logo_RGB_V2.png \
-  | magick - \
-  -trim \ # crop transparent edges
-  -channel RGB -threshold 100% \ # force cloud to all black, keeping transparency as is
-  -channel RGBA \ # reset to include transparency
-  -resize 'x26' \ # auto-width, height: 26px
-  -density 144 -units pixelsperinch \ # 144ppi
-  assets/icon.png
+# -trim: crop transparent edges
+# -channel RGB -threshold 100%: force cloud to all black, keeping transparency as is
+# -channel RGBA: reset to include transparency
+# -resize: auto-width, height: 26px
+curl -s https://c1.sfdcstatic.com/content/dam/web/en_us/www/images/styleguide/Logos/Salesforce_Logo_RGB_V2.png |
+  magick - \
+    -trim \
+    -channel RGB -threshold 100% \
+    -channel RGBA \
+    -resize 'x26' \
+    -density 144 -units pixelsperinch \
+    assets/icon.png
