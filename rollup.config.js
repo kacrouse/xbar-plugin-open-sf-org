c46b4e82b09c1a591222915a18da01a1d16e46b4
Kyle Crouse <crousek@us.ibm.com>
2020-06-11 12:02:19 -0600

    simple templating for bitbar metadata banner

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,10 @@ import path from "path";
 import babel from "@rollup/plugin-babel";
 import builtinModules from "builtin-modules";
 import fs from "fs";
+import packageJson from "./package.json";
+
+const render = (template, data) =>
+  template.replace(/\{\{(.+)\}\}/g, (_, key) => data[key] || "");
 
 export default {
   input: "src/index.js",
@@ -27,7 +31,21 @@ export default {
     babel({ babelHelpers: "bundled" }),
     {
       name: "bitbar-metadata-banner",
-      banner: fs.readFileSync(path.resolve(__dirname, "bitbar-metadata.js")),
+      banner: render(
+        fs.readFileSync(path.resolve(__dirname, "bitbar-metadata.js"), {
+          encoding: "utf8",
+        }),
+        {
+          title: packageJson.bitbar.title,
+          version: packageJson.version,
+          author: packageJson.author,
+          authorGithub: packageJson.bitbar.authorGithub,
+          desc: packageJson.bitbar.desc,
+          image: packageJson.bitbar.image,
+          dependencies: packageJson.bitbar.dependencies,
+          abouturl: packageJson.homepage,
+        }
+      ),
     },
   ],
   external: builtinModules,
