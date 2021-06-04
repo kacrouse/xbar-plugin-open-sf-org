9fe939eb5e28729ed386c98747259b598575649e
Kyle Crouse <kyle.crouse@gonimbly.com>
2021-06-04 16:45:12 -0600

    improve error reporting if command fails

--- src/salesforce.js
+++ src/salesforce.js
@@ -1,9 +1,15 @@
 import { findExecutable, runCommand } from "./util";
 
 const getOrgs = () => {
-  const orgListOutput = JSON.parse(
-    runCommand(`${findExecutable("sfdx")} force:org:list --json`)
+  const commandResult = runCommand(
+    `${findExecutable("sfdx")} force:org:list --json`
   );
+  const stdout = commandResult.stdout.toString();
+  const stderr = commandResult.stderr.toString();
+  if (!stdout && stderr) {
+    throw Error(stderr);
+  }
+  const orgListOutput = JSON.parse(stdout);
   if (orgListOutput.status && orgListOutput.status !== 0) {
     throw Error(
       "Error getting orgs through the Salesforce CLI. " +
